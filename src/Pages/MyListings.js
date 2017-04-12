f5ac037767b6c1436e845f3a34f317713cc087e0
Satyadev <satyadev.kadaganchi@gmail.com>
2017-04-12 08:03:31 +0200

    layout modified

--- src/Pages/MyListings.js
+++ src/Pages/MyListings.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import apiTask from '../api/task';
-import DOMPurify from 'dompurify';
-import { Link } from 'react-router'
 import Paper from 'material-ui/Paper';
-import Divider from 'material-ui/Divider';
-import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import * as coreNavigation from '../core/navigation';
 import LinearProgress from 'material-ui/LinearProgress';
-import {Card, CardTitle, CardMedia, CardText} from 'material-ui/Card';
+
 
 const style = {
     height: 350,
@@ -91,6 +87,7 @@ class MyListings extends Component {
                                                 <div className="col-xs-12 col-sm-12">
                                                      <h1 style={{'fontSize':'30px', 'marginTop':'0px'}} >In Progress</h1>
                                                  </div>
+                                            </div>  {/* this respective row is closed  */}
                                              <div className="row" >
                                              <div className="col-xs-12 col-sm-12"  >
                                                
@@ -118,15 +115,11 @@ class MyListings extends Component {
                                                         )    
                                                     }) 
                                                 }  
-                                                
-                                            </div>
-                                            
-                                               
-                                                </div>
-                                      </div>
-                                </div>
-                          </div>
-                    </div>
+                                                </div> {/* sm-12 closed */}
+                                      </div>   {/* row closed before sm-12 */}
+                                </div> {/* sm-8 close */}
+                          </div> {/* 1st row closed  */}
+                    </div> {/* container closed  */}
           </div>)
     }
 }

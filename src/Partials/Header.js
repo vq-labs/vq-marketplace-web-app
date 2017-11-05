517a06db1194e8213e9dc9205fbf013678fe5409
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-11-05 22:19:58 +0100

    Header for admin is different now

--- src/Partials/Header.js
+++ src/Partials/Header.js
@@ -7,6 +7,7 @@ import DashboardIcon from 'material-ui/svg-icons/action/dashboard';
 import SearchIcon from 'material-ui/svg-icons/action/search';
 import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import Logo from './Logo';
 import { Toolbar, ToolbarGroup, ToolbarSeparator } from 'material-ui/Toolbar';
 import Avatar from 'material-ui/Avatar';
@@ -15,6 +16,7 @@ import { translate } from '../core/i18n';
 import * as coreAuth from '../core/auth';
 import { goTo, goStartPage } from '../core/navigation';
 import * as DEFAULTS from '../constants/DEFAULTS';
+import { browserHistory } from 'react-router';
 
 const headerBtnStyle = {
   'marginRight': '0px',
@@ -54,6 +56,14 @@ class Header extends Component {
     });
   } 
 
+  componentDidMount() {
+    browserHistory.listen(location =>  {
+      this.setState({
+        shouldDisplay: location.pathname.indexOf("admin") == -1
+      })
+    });
+  }
+
   handleLogout(e) {
     e.preventDefault();
 
@@ -77,115 +87,126 @@ class Header extends Component {
                 appName={this.props.appName}
                 logo={this.props.logo}
               />
-              <ToolbarGroup>
-                        { this.state.logged &&
-                        <div onClick={ 
-                              () => { goTo('/dashboard'); 
-                        }}>
-                          <IconButton
-                            className="visible-xs"
-                            iconStyle={{ color: grey600 }}>
-                            <DashboardIcon />
-                          </IconButton>
+              { !this.state.shouldDisplay &&
+                <ToolbarGroup>
+                  <RaisedButton
+                    onTouchTap={() => goTo("/")}
+                    backgroundColor={"#000639"}
+                    labelColor={"white"}
+                    label={'Go to marketplace'}
+                  />
+                </ToolbarGroup>
+              }
+              { this.state.shouldDisplay &&
+                <ToolbarGroup>
+                          { this.state.logged &&
+                          <div onClick={ 
+                                () => { goTo('/dashboard'); 
+                          }}>
+                            <IconButton
+                              className="visible-xs"
+                              iconStyle={{ color: grey600 }}>
+                              <DashboardIcon />
+                            </IconButton>
 
-                          <FlatButton
-                            className="hidden-xs"
-                            label={translate("DASHBOARD")}
-                            
-                            style={headerBtnStyle}
-                          />
-                        </div>
-                        }
-                        { !this.state.logged &&
-                        <FlatButton label={translate("SIGNUP")} onClick={ 
-                          () => goTo('/signup')}
-                          style={headerBtnStyle} />
-                        }
-                        { !this.state.logged &&
-                        <FlatButton label={translate("LOGIN")} onClick={ 
-                          () => { goTo('/login'); 
-                        }} style={headerBtnStyle} />
-                        }
-                  { this.state.logged && <ToolbarSeparator /> }
-
-                  { this.state.logged && Number(this.state.user.userType) !== 1 &&
-                    <div onTouchTap={ 
-                        () => goTo('/')
-                    }>
-                      <IconButton
-                        className="visible-xs"
-                        iconStyle={{ color: grey600 }}>
-                        <SearchIcon />
-                      </IconButton>
-
-                      <FlatButton
-                        className="hidden-xs"
-                        label={translate('HEADER_LISTINGS')} 
-                        style={headerBtnStyle}
-                      />
-                    </div>
-                  }
-
-                  { this.state.logged && Number(this.state.user.userType) !== 2 &&
-                    <a onClick={() => goTo('/new-listing')} target="_self">
-                      {
-                        translate('HEADER_ADD_LISTING') === 'HEADER_ADD_LISTING' ?
-                        <IconButton iconStyle={{ color: grey600 }}>
-                          <ContentAdd />
-                        </IconButton> :
-                        <FlatButton 
-                            label={translate('HEADER_ADD_LISTING')}
-                            style={headerBtnStyle}
-                        />
-                      }
-                    </a>
-                  }
-              
-                { false && this.state.logged && 
-                  <IconButton iconStyle={{ color: grey600 }}  onClick={ () => { goTo('/chat' ) }}>
-                    <CommunicationChatBubble />
-                  </IconButton>
-                }
-
-                { this.state.logged &&
-                    <IconMenu
-                          style={{ cursor: 'pointer' }}
-                          iconButtonElement={
-                            <div>
-                              <Avatar src={this.state.user.imageUrl || DEFAULTS.PROFILE_IMG_URL} size={40} />
-                            </div>
+                            <FlatButton
+                              className="hidden-xs"
+                              label={translate("DASHBOARD")}
+                              
+                              style={headerBtnStyle}
+                            />
+                          </div>
                           }
-                          anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-                            targetOrigin={{horizontal: 'left', vertical: 'top'}}  >
-                      <MenuItem 
-                        onClick={
-                          () => goTo(`/profile/${this.state.user.id}`, (newPath, oldPath) => {
-                            if (oldPath.indexOf('profile') > -1) {
-                              return true;
-                            }
+                          { !this.state.logged &&
+                          <FlatButton label={translate("SIGNUP")} onClick={ 
+                            () => goTo('/signup')}
+                            style={headerBtnStyle} />
+                          }
+                          { !this.state.logged &&
+                          <FlatButton label={translate("LOGIN")} onClick={ 
+                            () => { goTo('/login'); 
+                          }} style={headerBtnStyle} />
+                          }
+                    { this.state.logged && <ToolbarSeparator /> }
 
-                            return false;
-                          })
+                    { this.state.logged && Number(this.state.user.userType) !== 1 &&
+                      <div onTouchTap={ 
+                          () => goTo('/')
+                      }>
+                        <IconButton
+                          className="visible-xs"
+                          iconStyle={{ color: grey600 }}>
+                          <SearchIcon />
+                        </IconButton>
+
+                        <FlatButton
+                          className="hidden-xs"
+                          label={translate('HEADER_LISTINGS')} 
+                          style={headerBtnStyle}
+                        />
+                      </div>
+                    }
+
+                    { this.state.logged && Number(this.state.user.userType) !== 2 &&
+                      <a onClick={() => goTo('/new-listing')} target="_self">
+                        {
+                          translate('HEADER_ADD_LISTING') === 'HEADER_ADD_LISTING' ?
+                          <IconButton iconStyle={{ color: grey600 }}>
+                            <ContentAdd />
+                          </IconButton> :
+                          <FlatButton 
+                              label={translate('HEADER_ADD_LISTING')}
+                              style={headerBtnStyle}
+                          />
                         }
-                        primaryText={translate("PROFILE")}
-                      />                 
-                      
-                     
-                      <MenuItem 
-                          onClick={() => goTo(`/account`)}
-                          primaryText={translate("ACCOUNT_SETTINGS")}
-                      />     
-                   
-                      { coreAuth.isAdmin() &&
-                        <MenuItem onClick={
-                          () => goTo('/admin/overview')
-                        } primaryText="Admin dashboard" /> 
-                      }
-                      <MenuItem onClick={this.handleLogout} primaryText="Logout" />
-                    </IconMenu>
-                    
+                      </a>
+                    }
+                
+                  { false && this.state.logged && 
+                    <IconButton iconStyle={{ color: grey600 }}  onClick={ () => { goTo('/chat' ) }}>
+                      <CommunicationChatBubble />
+                    </IconButton>
                   }
-              </ToolbarGroup>
+
+                  { this.state.logged &&
+                      <IconMenu
+                            style={{ cursor: 'pointer' }}
+                            iconButtonElement={
+                              <div>
+                                <Avatar src={this.state.user.imageUrl || DEFAULTS.PROFILE_IMG_URL} size={40} />
+                              </div>
+                            }
+                            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                              targetOrigin={{horizontal: 'left', vertical: 'top'}}  >
+                        <MenuItem 
+                          onClick={
+                            () => goTo(`/profile/${this.state.user.id}`, (newPath, oldPath) => {
+                              if (oldPath.indexOf('profile') > -1) {
+                                return true;
+                              }
+
+                              return false;
+                            })
+                          }
+                          primaryText={translate("PROFILE")}
+                        />                 
+                        
+                      
+                        <MenuItem 
+                            onClick={() => goTo(`/account`)}
+                            primaryText={translate("ACCOUNT_SETTINGS")}
+                        />     
+                    
+                        { coreAuth.isAdmin() &&
+                          <MenuItem onClick={
+                            () => goTo('/admin/overview')
+                          } primaryText="Admin dashboard" /> 
+                        }
+                        <MenuItem onClick={this.handleLogout} primaryText="Logout" />
+                      </IconMenu>
+                    }
+                </ToolbarGroup>
+              }
             </Toolbar>
         </div>
       );

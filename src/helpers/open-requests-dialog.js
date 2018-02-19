a453d3555a0e2197fa7eecff83a1c3c743ac2f08
Sercan AKMAN <sercan@tdsmaker.com>
2018-02-19 17:55:01 +0300

    requests dialog box reverted to non accept-booking stepped version (reverted from commit 233536ba7bcc532af68046d279bc9877e1a46fc4)

--- src/helpers/open-requests-dialog.js
+++ src/helpers/open-requests-dialog.js
@@ -24,7 +24,8 @@ export const Component = class RequestDialog extends React.Component {
         super();
         this.state = {
             isOpen: false,
-            requests: [],
+            pendingRequests: [],
+            acceptedRequests: [],
         };
     }
 
@@ -35,7 +36,8 @@ export const Component = class RequestDialog extends React.Component {
 
         onOpen = (requests, task) => {
             this.setState({
-                requests,
+                pendingRequests: requests.filter(_ => _.status === REQUEST_STATUS.PENDING),
+                acceptedRequests: requests.filter(_ => _.status === REQUEST_STATUS.ACCEPTED),
                 task,
                 isOpen: true
             });
@@ -59,16 +61,18 @@ export const Component = class RequestDialog extends React.Component {
                                     { this.state.isOpen &&
                                     <div className="row">
                                         <h1 style={{fontWeight: 'bold'}}>{translate('REQUESTS')}</h1>
-                                    
+                                        
+                                        <h4>{translate('PENDING_REQUESTS')}</h4>
                                         <List>
-                                        {this.state.requests
+                                        {this.state.pendingRequests
                                         .sort((a, b) => b.fromUser.avgReviewRate - a.fromUser.avgReviewRate )
                                         .map((request, index) =>
                                             <ListItem
                                                 key={index}
                                                 onTouchTap={() => {
                                                     this.setState({
-                                                        requests: [],
+                                                        pendingRequests: [],
+                                                        acceptedRequests: [],
                                                         isOpen: false
                                                     });
 
@@ -98,13 +102,74 @@ export const Component = class RequestDialog extends React.Component {
                                                 }
                                             />
                                         )}
-                                        {!this.state.requests
+                                        {!this.state.pendingRequests
                                         .length &&
                                         <p className="text-muted">
                                             {translate('NO_REQUESTS')}
                                         </p>
                                         }
-                                        </List>                                                                        
+                                        </List>
+
+                                        {
+                                            this.state.task &&
+                                            this.state.task.taskType === 2 &&
+                                            <div>
+                                                <br />
+                                                <Divider />
+                                                <br />
+                                                <h4>{translate('ACCEPTED_REQUESTS')}</h4>
+                                                <List>
+                                                {this.state.acceptedRequests
+                                                .sort((a, b) => b.fromUser.avgReviewRate - a.fromUser.avgReviewRate )
+                                                .map((request, index) =>
+                                                    <ListItem
+                                                        key={index}
+                                                        onTouchTap={() => {
+                                                            this.setState({
+                                                                pendingRequests: [],
+                                                                acceptedRequests: [],
+                                                                isOpen: false
+                                                            });
+
+                                                            return goTo(`/request/${request.id}`);
+                                                        }}
+                                                        primaryText={`${request.fromUser.firstName} ${request.fromUser.lastName}`}
+                                                        secondaryText={
+                                                            <p>
+                                                                {this.state.config &&
+                                                                    <Moment format={`${this.state.config.DATE_FORMAT}, ${this.state.config.TIME_FORMAT}`}>{request.createdAt}</Moment>
+                                                                }
+                                                            </p>
+                                                        }
+                                                        leftAvatar={<Avatar src={request.fromUser.imageUrl || DEFAULTS.PROFILE_IMG_URL} />}
+                                                        rightIcon={
+                                                            <div style={{ width: '60px' }}>
+                                                                <ReactStars
+                                                                    edit={false}
+                                                                    disable={true}
+                                                                    count={5}
+                                                                    size={12}
+                                                                    half={false}
+                                                                    value={request.fromUser.avgReviewRate}
+                                                                    color2={'#ffd700'}
+                                                                />
+                                                            </div>
+                                                        }
+                                                    />
+                                                )}
+                                                {!this.state.acceptedRequests
+                                                .length &&
+                                                <p className="text-muted">
+                                                    {translate('NO_REQUESTS')}
+                                                </p>
+                                                }
+                                                </List>
+                                            </div>
+                                        }
+
+                                        
+
+                                        
                                     </div>
                                     }
                                 </Dialog>
